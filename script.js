bc99fa86a988d1c9a895b166971bf2651282723e
Jay Patel <68432630+JP8433@users.noreply.github.com>
2022-11-01 17:50:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var contanier = document.getElementById('container')
+setTimeout(() => {
+    contanier.innerHTML = `<p>10</p>`;
+ setTimeout(() => {
+ contanier.innerHTML = `<p>9</p>`; 
+      setTimeout(() => {
+  contanier.innerHTML = `<p>8</p>`;
+ setTimeout(() => {
+     contanier.innerHTML = `<p>7</p>`; 
+      setTimeout(() => {
+     contanier.innerHTML = `<p>6</p>`;
+     setTimeout(() => {
+     contanier.innerHTML = `<p>5</p>`;
+     setTimeout(() => {
+      contanier.innerHTML = `<p>4</p>`;
+     setTimeout(() => {
+     contanier.innerHTML = `<p>3</p>`;
+     setTimeout(() => {
+        contanier.innerHTML = `<p>2</p>`;
+        setTimeout(() => {
+    contanier.innerHTML = `<p>1</p>`;
+     setTimeout(() => {
+     contanier.innerHTML = ` <img src="https://img.freepik.com/free-vector/15th-august-happy-independence-day-india_1017-20000.jpg?w=900&t=st=1667305077~exp=1667305677~hmac=0c0559af0d76bea0a3aee90e0890bdc73d7b3e21d619d7b25424e85094e6c2af">`;
+     },1500);
+      },1500);
+     },1500);
+     },1500);
+      },1500);
+        }, 1500);
+      }, 1500);
+     }, 1500);
+     }, 1500);
+}, 1500);
+},0);
